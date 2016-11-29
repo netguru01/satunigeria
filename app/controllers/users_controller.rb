c28e93d5f2d108483226f9fde142ae931d3510e6
Yinka Ash <slankeyinc@gmail.com>
2016-11-29 12:01:07 +0000

    Create New & Edit forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,27 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "Welcome to Satu Nigeria Blog #{@user.username}"
+            flash[:success] = "Welcome to Satu Nigeria Blog, #{@user.username}"
             redirect_to articles_path
         else
             render 'new'
         end
     end
     
+    def edit
+        @user = User.find(params[:id])
+        
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your details has been successfully updated!"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
     
     
 private
